08838ad0b549cc396780b733d61953ea82309f75
leider diaz <leidermartinez2012@gmail.com>
2025-03-28 23:30:39 -0500

    commit

--- consumer.py
+++ consumer.py
@@ -1,4 +1,5 @@
 import pika
+import logging
 
 # Conexión a RabbitMQ usando la IP del servidor RabbitMQ
 connection = pika.BlockingConnection(
@@ -9,12 +10,12 @@ channel = connection.channel()
 #channel.queue_declare(queue='cola5')
 
 def callback(ch, method, properties, body):
-    print("Mensaje recibido:", body.decode())
+    logging.info("Mensaje recibido:", body.decode())
 
 # Suscribirse a la cola
 channel.basic_consume(queue='cola5',
                       on_message_callback=callback,
                       auto_ack=True)
 
-print('Esperando mensajes. Presiona CTRL+C para salir')
+logging.info('Esperando mensajes. Presiona CTRL+C para salir')
 channel.start_consuming()
